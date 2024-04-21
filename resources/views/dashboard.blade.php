967edf56993f10d15013d6f65031a36788f02c34
Rakesh4584 <Rakeshnandakumar2005@gmail.com>
2024-04-21 12:05:06 +0100

    Your commit message here

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+            <div class="p-6 flex bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="pt-2 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
+                <a href="{{route('home')}}" class="bg-teal-600 text-white rounded py-2 px-4 mx-2">Click to Continue</a>
             </div>
         </div>
     </div>
